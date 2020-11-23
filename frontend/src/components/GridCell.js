1ae815cdb1b84910287120d3a080c5ef4d0126c3
Yarden Lawson <yardenlawson@gmail.com>
2020-11-23 17:45:11 +0000

    update

--- frontend/src/components/GridCell.js
+++ frontend/src/components/GridCell.js
@@ -1,23 +1,26 @@
 import React from 'react'
 
-import roverdog from '../assets/roverdog.png'
+import roverN from '../assets/roverN.png'
+import roverE from '../assets/roverE.png'
+import roverS from '../assets/roverS.png'
+import roverW from '../assets/roverW.png'
 
 
 const GridCell = (props) => {
     const { cell, rover, handleClick, isClicked } = props
     
-
+    let roverImgPos
     //* assigning the correlating image position depending on the rover's movement assignement
     // let roverImgPos
     if (rover !== undefined) {
         if (rover.currentPosition.position === 'E') {
-            // roverImgPos = roverE
+            roverImgPos = roverE
         } else if (rover.currentPosition.position === 'N') {
-            // roverImgPos = roverN
+            roverImgPos = roverN
         } else if (rover.currentPosition.position === 'S') {
-            // roverImgPos = roverS
+            roverImgPos = roverS
         } else if (rover.currentPosition.position === 'W') {
-            // roverImgPos = roverW
+            roverImgPos = roverW
         }
     }
 
@@ -30,7 +33,7 @@ const GridCell = (props) => {
                 <div className="box-root">
                     {rover !== undefined ?
                         <div className="rover-wrapper">
-                            <img className="roverdog" src={roverdog} rover_id={rover.roverId} cell_x={cell.x} cell_y={cell.y} onClick={(e) => handleClick(e)} alt='roverdog'/>
+                            <img className="roverdog" src={roverImgPos} rover_id={rover.roverId} cell_x={cell.x} cell_y={cell.y} onClick={(e) => handleClick(e)} alt='roverdog'/>
                         </div> : null}
                 </div>
             </div>
